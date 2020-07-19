5543570dffa76afc054a0f99af4669715228a359
Glitch (roborallydb) <none>
2020-07-19 20:45:25 +0000

    Actions table now has four columns (card text, priority, action, unit). Routes created to create the table, drop the table, populate the table with rows for each card in the actual game.

--- public/client.js
+++ public/client.js
@@ -1,51 +1,51 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 
-
 const actions = [];
 
 // define variables that reference elements on our page
-const actionsForm = document.forms[0];
-const actionInput = actionsForm.elements["action"];
-const priorityInput = actionsForm.elements["priority"];
 const actionsList = document.getElementById("actions");
-const clearButton = document.querySelector('#clear-actions');
+const clearButton = document.querySelector("#clear-actions");
+const reloadButton = document.querySelector("#reload-actions");
 
-// request the actions from our app's sqlite database
-fetch("/getActions", {})
-  .then(res => res.json())
-  .then(response => {
-    response.forEach(row => {
-      appendNewAction(row.action + ": " + row.priority);
+function fillActions() {
+  // request the actions from our app's sqlite database
+  fetch("/getActions", {})
+    .then(res => res.json())
+    .then(response => {
+      response.forEach(row => {
+        appendNewAction("[" + row.id + "] " +row.cardText + ": " + row.priority + " (" + row.action + " " + row.unit + ")");
+      });
     });
-  });
+}
 
-let selected = null
+fillActions();
+let selected = null;
 
 function dragOver(e) {
   if (isBefore(selected, e.target)) {
-    e.target.parentNode.insertBefore(selected, e.target)
+    e.target.parentNode.insertBefore(selected, e.target);
   } else {
-    e.target.parentNode.insertBefore(selected, e.target.nextSibling)
+    e.target.parentNode.insertBefore(selected, e.target.nextSibling);
   }
 }
 
 function dragEnd() {
   console.log("dragEnd");
-  selected = null
+  selected = null;
 }
 
 function dragStart(e) {
-  e.dataTransfer.effectAllowed = 'move'
-  e.dataTransfer.setData('text/plain', null)
-  selected = e.target
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/plain", null);
+  selected = e.target;
 }
 
 function isBefore(el1, el2) {
-  let cur
+  let cur;
   if (el2.parentNode === el1.parentNode) {
     for (cur = el1.previousSibling; cur; cur = cur.previousSibling) {
-      if (cur === el2) return true
+      if (cur === el2) return true;
     }
   }
   return false;
@@ -56,59 +56,38 @@ const appendNewAction = action => {
   const newListItem = document.createElement("li");
   newListItem.innerText = action;
   actionsList.appendChild(newListItem);
-  
-  let draggable = document.createAttribute("draggable");       
-  draggable.value = "true";           
-  newListItem.setAttributeNode(draggable);        
-  
-  let dragend = document.createAttribute("ondragend");       
-  dragend.value = "dragEnd()";           
-  newListItem.setAttributeNode(dragend);   
-  
-  let dragover = document.createAttribute("ondragover");       
-  dragover.value = "dragOver(event)";           
-  newListItem.setAttributeNode(dragover);   
- 
-  let dragstart = document.createAttribute("ondragstart");       
-  dragstart.value = "dragStart(event)";           
-  newListItem.setAttributeNode(dragstart);   
-/*
 
-  ondragend="dragEnd()" ondragover="dragOver(event)" ondragstart="dragStart(event)"
-*/  
+  let draggable = document.createAttribute("draggable");
+  draggable.value = "true";
+  newListItem.setAttributeNode(draggable);
+
+  let dragend = document.createAttribute("ondragend");
+  dragend.value = "dragEnd()";
+  newListItem.setAttributeNode(dragend);
+
+  let dragover = document.createAttribute("ondragover");
+  dragover.value = "dragOver(event)";
+  newListItem.setAttributeNode(dragover);
+
+  let dragstart = document.createAttribute("ondragstart");
+  dragstart.value = "dragStart(event)";
+  newListItem.setAttributeNode(dragstart);
 };
 
-// listen for the form to be submitted and add a new action when it is
-actionsForm.onsubmit = event => {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  const data = { action: actionInput.value, priority: priorityInput.value };
-
-  fetch("/addAction", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
-  })
-    .then(res => res.json())
-    .then(response => {
-      console.log(JSON.stringify(response));
-    });
-  // get action value and add it to the list
-  actions.push(actionInput.value);
-  appendNewAction(actionInput.value + ": " + priorityInput.value);
-
-  // reset form
-  actionInput.value = "";
-  priorityInput.value = "";
-  actionInput.focus();
-};
-
-clearButton.addEventListener('click', event => {
+clearButton.addEventListener("click", event => {
   fetch("/clearActions", {})
     .then(res => res.json())
     .then(response => {
       console.log("cleared actions");
     });
   actionsList.innerHTML = "";
+});
+
+reloadButton.addEventListener("click", async event => {
+  await fetch("/loadDefaultActions", {})
+    .then(res => res.json())
+    .then(response => {
+      console.log("reloaded actions");
+    });
+  fillActions();
 });
--- server.js
+++ server.js
@@ -23,24 +23,21 @@ const db = new sqlite3.Database(dbFile);
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(() => {
-  if (!exists) {    
+  if (!exists) {
     db.run(
-      "CREATE TABLE IF NOT EXISTS Actions (id INTEGER PRIMARY KEY AUTOINCREMENT, action TEXT, priority INTEGER)"
-    )
+      "CREATE TABLE IF NOT EXISTS Actions (id INTEGER PRIMARY KEY AUTOINCREMENT, cardText TEXT, priority INTEGER, action TEXT, unit INTEGER)"
+    );
     console.log("New table Actions created!");
-
-    
   } else {
     console.log('Database "Actions" ready to go!');
 
     db.each("SELECT * from Actions", (err, row) => {
       if (row) {
-        console.log(`record: ${row.action}, ${row.priority}` );
+        console.log(`record: ${row.action}, ${row.priority}`);
       }
     });
   }
 });
-
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
@@ -52,25 +49,6 @@ app.get("/getActions", (request, response) => {
   db.all("SELECT * from Actions", (err, rows) => {
     response.send(JSON.stringify(rows));
   });
-});
-
-// endpoint to add a dream to the database
-app.post("/addAction", (request, response) => {
-  console.log(`add to actions ${request.body.action}`);
-
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects
-  // so they can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    const cleansedAction = cleanseString(request.body.action);
-    console.log("insert:", cleansedAction, " with priority:", request.body.priority);
-    db.run(`INSERT INTO Actions (action, priority) VALUES (?, ?)`, [cleansedAction, request.body.priority], error => {
-      if (error) {
-        response.send({ message: "error!" });
-      } else {
-        response.send({ message: "success" });
-      }
-    });
-  }
 });
 
 // endpoint to clear dreams from the database
@@ -98,50 +76,79 @@ app.get("/clearActions", (request, response) => {
   }
 });
 
-app.get("/loadDefaultActions", (request, response) => {
- // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-   
-  let actions = [
-     ["Move 3", 830],
-     ["Move 3", 780],
-     ["Move 2", 660],
-     ["Move 3", 680],
-  ];
-
-    console.log(JSON.stringify(actions));
-
-  // create the statement for the insertion of just ONE record
-  let actionQuery = 
-     "INSERT INTO Actions (action, priority) " +
-     "VALUES (?, ?)"; 
-
-  // 'prepare' returns a 'statement' object which allows us to 
-  // bind the same query to different parameters each time we run it
-  let statement = db.prepare(actionQuery);
-
-  // run the query over and over for each inner array
-  for (var i = 0; i < actions.length; i++) {
-      statement.run(actions[i], err => {
-               
-      });
-  }
-    
-  // 'finalize' basically kills our ability to call .run(...) on the 'statement'
-  // object again. Optional.
-  statement.finalize();
-
-  response.send({ message: "success" });    
-  // If I call statement.run( ... ) here again, I will get an error due 
-  // to the 'finalize' call above.      
-  }
+app.get("/createActions", (request, response) => {
+  db.serialize(() => {
+    db.run(
+      "CREATE TABLE IF NOT EXISTS Actions (id INTEGER PRIMARY KEY AUTOINCREMENT, cardText TEXT, priority INTEGER, action TEXT, unit INTEGER)"
+    );
+  });
+  response.send({ message: "success" });
+  console.log("New table Actions created!");
 });
 
+app.get("/dropActions", (request, response) => {
+  db.serialize(() => {
+    db.run("DROP TABLE Actions");
+  });
+  response.send({ message: "success" });
+  console.log("table Actions deleted!");
+});
 
-// helper function that prevents html/css/script malice
-const cleanseString = function(string) {
-  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
+function genActionsArray(
+  cardText,
+  priorityStart,
+  priorityEnd,
+  priorityIncrement,
+  action,
+  unit
+) {
+  var actionsArray = [];
+  for (
+    var priority = priorityStart;
+    priority <= priorityEnd;
+    priority += priorityIncrement
+  ) {
+    actionsArray.push([cardText, priority, action, unit]);
+  }
+
+  return actionsArray;
+}
+
+app.get("/loadDefaultActions", (request, response) => {
+  // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
+  if (!process.env.DISALLOW_WRITE) {
+    let actions = genActionsArray("U-Turn", 10, 60, 10, "Rotate", 180)
+      .concat(genActionsArray("Rotate Right", 80, 420, 20, "Rotate", 90))
+      .concat(genActionsArray("Rotate Left", 70, 410, 20, "Rotate", -90))
+      .concat(genActionsArray("Back Up", 430, 480, 10, "Step", -1))
+      .concat(genActionsArray("Move 1", 430, 480, 10, "Step", 1))
+      .concat(genActionsArray("Move 2", 490, 780, 10, "Step", 2))
+      .concat(genActionsArray("Move 3", 790, 840, 10, "Step", 3));
+    console.log(JSON.stringify(actions));
+
+    // create the statement for the insertion of just ONE record
+    let actionQuery =
+      "INSERT INTO Actions (cardText, priority, action, unit) " +
+      "VALUES (?, ?, ?, ?)";
+
+    // 'prepare' returns a 'statement' object which allows us to
+    // bind the same query to different parameters each time we run it
+    let statement = db.prepare(actionQuery);
+
+    // run the query over and over for each inner array
+    for (var i = 0; i < actions.length; i++) {
+      statement.run(actions[i], err => {});
+    }
+
+    // 'finalize' basically kills our ability to call .run(...) on the 'statement'
+    // object again. Optional.
+    statement.finalize();
+
+    response.send({ message: "success" });
+    // If I call statement.run( ... ) here again, I will get an error due
+    // to the 'finalize' call above.
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
